a2b2f4ef326536e7ce3464bf567a31a316872854
Alex Rolfe <rolfepercussion@gmail.com>
2015-12-07 12:43:47 -0600

    Checkpoint 20 changes completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,7 +1,36 @@
+require_relative 'entry'
+
 class AddressBook
   attr_accessor :entries
 
   def initialize
     @entries = []
+  end
+
+  def add_entry(name, phone_number, email)
+
+    index = 0
+    @entries.each do |entry|
+
+      if name < entry.name
+        break
+      end
+      index += 1
+    end
+
+    @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def remove_entry(name, phone_number, email)
+
+    remove_index = 0
+    @entries.each do |entry|
+
+      if name == entry.name
+        @entries.delete(entries[remove_index])
+          break
+      end
+      remove_index += 1
+    end
   end
 end
