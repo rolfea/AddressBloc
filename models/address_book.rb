2d8c807836004d441e37879988104b9965276935
Alex Rolfe <rolfepercussion@gmail.com>
2015-12-08 22:05:43 -0600

    Checkpoint 22 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,7 +9,6 @@ class AddressBook
   end
 
   def add_entry(name, phone_number, email)
-
     index = 0
     @entries.each do |entry|
 
@@ -18,7 +17,6 @@ class AddressBook
       end
       index += 1
     end
-
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
 
@@ -33,15 +31,33 @@ class AddressBook
   end
 
   def remove_entry(name, phone_number, email)
-
     remove_index = 0
     @entries.each do |entry|
-
       if name == entry.name
         @entries.delete(entries[remove_index])
           break
       end
       remove_index += 1
     end
+  end
+
+  def binary_search(name)
+    lower = 0
+    upper = @entries.length - 1
+
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = @entries[mid].name
+
+      if name == mid_name
+        return @entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    return nil
   end
 end
